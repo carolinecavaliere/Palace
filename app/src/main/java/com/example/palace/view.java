fb990b1481fbd1df813a27247b1fb7342bdd815d
Nathaniel <pon23@up.edu>
2020-09-21 18:42:48 -0700

    cards are oriented accordingly

--- app/src/main/java/com/example/palace/view.java
+++ app/src/main/java/com/example/palace/view.java
@@ -15,6 +15,10 @@ public class view extends SurfaceView {
     Paint textPaint = new Paint();
     Paint cardOutlinePaint = new Paint();
     float displayConvert = getResources().getDisplayMetrics().density;
+    float cardWidth = displayConvert * 75;
+    float cardHeight = displayConvert * 100;
+
+
     /**
      External Citation
      Date: 19 September 2020
@@ -33,7 +37,7 @@ public class view extends SurfaceView {
         cardBackPaint.setStyle(Paint.Style.FILL);
         cardBackPaint.setColor(Color.RED);
         textPaint.setColor(Color.BLACK);
-        textPaint.setTextSize(displayConvert * 20f);
+        textPaint.setTextSize(displayConvert *20f);
         textPaint.setTypeface(Typeface.SANS_SERIF);
         cardOutlinePaint.setColor(Color.BLACK);
         cardOutlinePaint.setStyle(Paint.Style.STROKE);
@@ -45,34 +49,54 @@ public class view extends SurfaceView {
         setBackgroundColor(0xFF31B94D);
         super.onDraw(canvas);
 
-        float x = (canvas.getWidth()/2f);
-        float y = (canvas.getHeight()/2f);
+        float xCenter = (canvas.getWidth()/2f);
+        float yCenter = (canvas.getHeight()/2f);
+        float xRight = canvas.getHeight();
+        float yBottom = canvas.getHeight();
+        float xMargin = displayConvert*100;
+        float yMargin = displayConvert*160;
 
-        drawCard(canvas, x, y, 10);
+        //draw deck and pile
+        drawCard(canvas, xCenter + 120 - cardWidth/2, yCenter, -50);
+        drawCard(canvas, xCenter - 120 - cardWidth/2, yCenter, 10);
 
+
+        //draw opponent's cards
+        drawCard(canvas, xCenter - cardWidth/2, yMargin, 10);
+        drawCard(canvas, xCenter + cardWidth *2 - cardWidth/2, yMargin, 10);
+        drawCard(canvas, xCenter - cardWidth *2 - cardWidth/2, yMargin, 10);
+        drawCard(canvas, xCenter + cardWidth * 4 - cardWidth/2, yMargin, -3);
+
+        //draw player's cards
+        drawCard(canvas, xCenter - cardWidth/2, yBottom - yMargin, 10);
+        drawCard(canvas, xCenter + cardWidth *2 - cardWidth/2, yBottom - yMargin, 10);
+        drawCard(canvas, xCenter - cardWidth *2 - cardWidth/2, yBottom - yMargin, 10);
     }
 
     public void drawCard(Canvas canvas, float left, float top, int rank) {
         canvas.drawRect(left,top, (left + displayConvert*75), (top + displayConvert*100), cardOutlinePaint);
-        if(rank==-1){
-            canvas.drawRect(left, top, (displayConvert*75) + left, (displayConvert * 100) + top, cardBackPaint);
+        if(rank <= -1){
+            canvas.drawRect(left, top, cardWidth + left, cardHeight + top, cardBackPaint);
+            textPaint.setTextSize(displayConvert*40f);
+            canvas.drawText(""+ (-1)*rank, left + cardWidth/3,top + cardHeight/2, textPaint);
+            textPaint.setTextSize(displayConvert *20f);
         }
         else {
-            canvas.drawRect(left, top, (displayConvert*75) + left, (displayConvert * 100) + top, cardPaint);
+            canvas.drawRect(left, top, cardWidth + left, cardHeight + top, cardPaint);
             if(rank>1&&rank<11){//standard cards
-                canvas.drawText(""+rank,(displayConvert*10) + left,(displayConvert*25) + top,textPaint);
+                canvas.drawText(""+ rank,displayConvert*10 + left,displayConvert*25 + top,textPaint);
             }
             else if(rank==11){
-                canvas.drawText("J",(displayConvert*10) + left,(displayConvert*25)+top,textPaint);
+                canvas.drawText("J",displayConvert*(left+10),displayConvert*(top+25),textPaint);
             }
             else if(rank==12){
-                canvas.drawText("Q",(displayConvert*10) + left,(displayConvert*25)+top,textPaint);
+                canvas.drawText("Q",displayConvert*(left+10),displayConvert*(top+25),textPaint);
             }
             else if(rank==13){
-                canvas.drawText("K",(displayConvert*10) + left,(displayConvert*25)+top,textPaint);
+                canvas.drawText("K",displayConvert*(left+10),displayConvert*(top+25),textPaint);
             }
             else if(rank==14){
-                canvas.drawText("A",(displayConvert*10) + left,(displayConvert*25)+top,textPaint);
+                canvas.drawText("A",displayConvert*(left+10),displayConvert*(top+25),textPaint);
             }
         }
         setBackgroundColor(0xFF31B94D);
